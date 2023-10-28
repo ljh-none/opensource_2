562990224b3bcc8b7c624282a6a40dbfa5d46e1f
ljh09-none <ljh09210921@gmail.com>
2023-10-28 13:32:32 +0900

    일단 틀은 어느정도 만듬

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,62 @@
 
 import 'package:flutter/material.dart';
 
+//페이지 이동을 위한 상수 및 함수
+const String HOMESUB = "homesub";
+const String CHATSUB = "chatsub";
+const String FLOATSUB = "floatsub";
+const String CATEGORYSUB = "categorysub";
+const String ALERTSUB = "alertsub";
+const String SEARCHSUB = "searchsub";
+
+Function gotoSub = (BuildContext context, String cls) {
+  switch (cls) {
+    case HOMESUB:
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const HomePageSub()));
+      break;
+    case CHATSUB:
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const ChatPageSub()));
+      break;
+    case FLOATSUB:
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const FloatingSub()));
+      break;
+    case CATEGORYSUB:
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const CategorySub()));
+      break;
+    case ALERTSUB:
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const AlertSub()));
+      break;
+    case SEARCHSUB:
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const SearchSub()));
+      break;
+    default:
+      break;
+  }
+};
+
+//데이터 클래스
+class ItemData {
+  var _item;
+  var _user;
+  var _category;
+  var _regitime;
+}
+
+class userData {
+  var _name;
+  var _location;
+}
+
+class SettingData {
+  var _filter;
+}
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,6 +80,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//Read Me
+//1. 메인 화면 구성
+//하단 바를 통한 페이지 이동 시 navigation pop 없이 이동하고 싶다.
+//우선 메인 화면에 상단바, 하단바, 플로팅 버튼으로 구성된 프레임을 만든다.
+//하단 바를 클릭할 때마다 body부분만 바뀐다 -> homepage, chatpage, settingpage
+//2. 서브 화면 구성 -> navigation pop
+// - home, chat, setting 페이지의 아이템 클릭 시
+// - appbar의 카테고리, 검색, 알람 버튼 클릭 시
+// - floating button 클릭 시
+//3. 다이얼로그 화면 구성
+// - location 버튼 클릭 시
+////////////////////////////메인 프레임////////////////////////////////////////////////////////
 class MainFrame extends StatefulWidget {
   const MainFrame({super.key});
   @override
@@ -66,12 +134,12 @@ class _MainFrameState extends State<MainFrame> {
       //barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Search Location"),
+          title: const Text("Search Location"),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(),
-              ElevatedButton(onPressed: nothing, child: Text("search")),
+              const TextField(),
+              ElevatedButton(onPressed: nothing, child: const Text("search")),
             ],
           ),
         );
@@ -88,10 +156,20 @@ class _MainFrameState extends State<MainFrame> {
           ElevatedButton(onPressed: isLocation, child: const Text("location")),
           const Spacer(),
           IconButton(
-              onPressed: nothing, icon: const Icon(Icons.format_align_justify)),
-          IconButton(onPressed: nothing, icon: const Icon(Icons.search)),
+              onPressed: () {
+                gotoSub(context, CATEGORYSUB);
+              },
+              icon: const Icon(Icons.format_align_justify)),
           IconButton(
-              onPressed: nothing, icon: const Icon(Icons.add_alert_rounded)),
+              onPressed: () {
+                gotoSub(context, SEARCHSUB);
+              },
+              icon: const Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                gotoSub(context, ALERTSUB);
+              },
+              icon: const Icon(Icons.add_alert_rounded)),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -106,11 +184,15 @@ class _MainFrameState extends State<MainFrame> {
         const ChatPage(),
         const SettingPage(),
       ][_bottomIndex],
-      floatingActionButton: FloatingActionButton(onPressed: nothing),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        gotoSub(context, FLOATSUB);
+      }),
     );
   }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////홈페이지/////////////////////////////////////////////
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
   @override
@@ -129,6 +211,9 @@ class _HomePageState extends State<HomePage> {
         title: Text("item $index"),
         subtitle:
             const Row(children: [Text("Location"), Spacer(), Text("time")]),
+        onTap: () {
+          gotoSub(context, HOMESUB);
+        },
       ),
     );
   }
@@ -143,6 +228,25 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class HomePageSub extends StatefulWidget {
+  const HomePageSub({super.key});
+  @override
+  State<HomePageSub> createState() => _HomePageSubState();
+}
+
+class _HomePageSubState extends State<HomePageSub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Item Page"),
+      ),
+    );
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////채팅 페이지/////////////////////////////////////////////
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
   @override
@@ -156,11 +260,13 @@ class _ChatPageState extends State<ChatPage> {
       color: Colors.orange.shade50,
       margin: const EdgeInsets.symmetric(vertical: 1),
       child: ListTile(
-        leading: Image.asset("images/EmptyProfileImage.png"),
-        title: Text("user $index"),
-        subtitle:
-            const Row(children: [Text("Location"), Spacer(), Text("time")]),
-      ),
+          leading: Image.asset("images/EmptyProfileImage.png"),
+          title: Text("user $index"),
+          subtitle:
+              const Row(children: [Text("Location"), Spacer(), Text("time")]),
+          onTap: () {
+            gotoSub(context, CHATSUB);
+          }),
     );
   }
 
@@ -173,6 +279,25 @@ class _ChatPageState extends State<ChatPage> {
   }
 }
 
+class ChatPageSub extends StatefulWidget {
+  const ChatPageSub({super.key});
+  @override
+  State<ChatPageSub> createState() => _ChatPageSubState();
+}
+
+class _ChatPageSubState extends State<ChatPageSub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User name"),
+      ),
+    );
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////세팅 페이지/////////////////////////////////////////////
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
   @override
@@ -184,5 +309,74 @@ class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return const Text("setting");
+  }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+class FloatingSub extends StatefulWidget {
+  const FloatingSub({super.key});
+  @override
+  State<FloatingSub> createState() => _FloatingSubState();
+}
+
+class _FloatingSubState extends State<FloatingSub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Item add"),
+      ),
+    );
+  }
+}
+
+class CategorySub extends StatefulWidget {
+  const CategorySub({super.key});
+  @override
+  State<CategorySub> createState() => _CategorySubState();
+}
+
+class _CategorySubState extends State<CategorySub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("category page"),
+      ),
+    );
+  }
+}
+
+class SearchSub extends StatefulWidget {
+  const SearchSub({super.key});
+  @override
+  State<SearchSub> createState() => _SearchSubState();
+}
+
+class _SearchSubState extends State<SearchSub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Search page"),
+      ),
+    );
+  }
+}
+
+class AlertSub extends StatefulWidget {
+  const AlertSub({super.key});
+  @override
+  State<AlertSub> createState() => _AlertSubState();
+}
+
+class _AlertSubState extends State<AlertSub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alert page"),
+      ),
+    );
   }
 }
